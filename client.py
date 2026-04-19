3e304e03b1b3bc682d98ede4332c8521f6bf6c1b
Adam Secrest <adam.secrest@csu.fullerton.edu>
2026-04-19 00:38:10 -0700

    Added all helpers outside of send and receive

--- client.py
+++ client.py
@@ -28,16 +28,36 @@ def recv_exact(sock, num_bytes):
         data += chunk
     return data
 
-def fileAccess(filename):
+def recv_message(sock):
+    header = recv_exact(sock, HEADER_SIZE)
+    if not header:
+        return None
     try:
-        with open(filename, "rb") as file:
-            sendableBytes = file.read()
-            return sendableBytes
-    except:
-        print("Invalid file")
+        messageLength = int(header.decode().strip())
+    except ValueError:
+        return None
+    return recv_exact(sock, messageLength)
 
+def data_listener():
+    listener = socket(AF_INET, SOCK_STREAM)
+    listener.bind("", 0)
+    listener.listen(1)
+    port = listener.getsockname()[1]
+    return listener, port
 
-clientSocket.connect((serverName, serverPort))
+def receive_listening(listener):
+    dataConnection, _ = listener.accept()
+    payload = recv_message(dataConnection)
+    dataConnection.close()
+    listener.close()
+    if payload is None:
+        print("Error while listening")
+        return
+    text = payload.decode()
+    if text:
+        print(text, end="")
+    else:
+        print("No files on the server")
 
 while True:
     command = str(input(">> "))
